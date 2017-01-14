217ea1e1ad0bd9a528ecef0b736f339977b2bfc0
Hunter <hantek.f@gmail.com>
2017-01-14 02:51:29 +0100

    409 to 404

--- tests/integration_DeleteItem.js
+++ tests/integration_DeleteItem.js
@@ -26,13 +26,13 @@ describe('REST API', () => {
     },
   };
 
-  context('when trying to insert an item into a user basket', () => {
-    it('it should reply with the current basket for successfully added items', () => {
+  context('when trying to delete an item into a user basket', () => {
+    it('it should reply with the current basket for successfully deleted items', () => {
       let opt = JSON.parse(JSON.stringify(options));
       opt.url += 'huw/fairphone17658';
       return server.inject(opt).then((response) => {
         response.should.be.an('object').and.contain.keys('statusCode', 'payload', 'headers');
-        response.statusCode.should.equal(200);
+        response.statusCode.should.equal(404);
         response.payload.should.be.a('string');
         let payload = JSON.parse(response.payload);
         payload.should.be.an('object').and.contain.keys('fairphone17658');
@@ -40,12 +40,12 @@ describe('REST API', () => {
       });
     });
 
-    it('it should reply with 409 for already existing items', () => {
+    it('it should reply with 404 for already deleted items', () => {
       let opt = JSON.parse(JSON.stringify(options));
       opt.url += 'huw/fairphone17658';
       return server.inject(opt).then((response) => {
         response.should.be.an('object').and.contain.keys('statusCode', 'payload');
-        response.statusCode.should.equal(409);
+        response.statusCode.should.equal(404);
         response.payload.should.be.a('string');
         let payload = JSON.parse(response.payload);
         payload.should.be.an('object').and.contain.keys('statusCode', 'error');
